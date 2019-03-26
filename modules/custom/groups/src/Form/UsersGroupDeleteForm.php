94b18b4c4aa352e6e8a849e36bab537ec81711f3
Arturas Raguckas <arturas.raguckas@webpartners.lt>
2019-03-26 10:52:35 +0200

    Users group entity delete validation

--- modules/custom/groups/src/Form/UsersGroupDeleteForm.php
+++ modules/custom/groups/src/Form/UsersGroupDeleteForm.php
@@ -3,6 +3,8 @@
 namespace Drupal\groups\Form;
 
 use Drupal\Core\Entity\ContentEntityDeleteForm;
+use Drupal\Core\Form\FormStateInterface;
+
 
 /**
  * Provides a form for deleting Users group entities.
@@ -11,5 +13,12 @@ use Drupal\Core\Entity\ContentEntityDeleteForm;
  */
 class UsersGroupDeleteForm extends ContentEntityDeleteForm {
 
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+
+    if ($form_state->getBuildInfo()['callback_object']->getEntity()->get('members')->getValue() != NULL) {
+      $form_state->setErrorByName('members', 'Not empty group. Delete all members');
+    }
+  }
 
 }
